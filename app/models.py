fc641bd60e4d9e64ac777f21bb81ebea55f84365
Jonathan Chow <jonathan.chow@mclaren.com>
2020-07-25 15:58:34 +0100

    Added changes from chapter 8

--- app/models.py
+++ app/models.py
@@ -8,6 +8,11 @@ from hashlib import md5
 def load_user(id):
     return User.query.get(int(id))
 
+followers = db.Table('followers',
+                         db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                         db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+                         )
+
 class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -15,10 +20,7 @@ class User(UserMixin,db.Model):
     password_hash = db.Column(db.String(128))
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
-    followers = db.Table('followers',
-                         db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-                         db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
-                         )
+
     followed = db.relationship(
         'User', secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
@@ -37,6 +39,24 @@ class User(UserMixin,db.Model):
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        return Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id).order_by(
+                    Post.timestamp.desc())
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
