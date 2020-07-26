f2a2537b068f263187d6003b4d4b143d80a9953f
Jonathan Chow <jonathan.chow@mclaren.com>
2020-07-26 16:40:41 +0100

    Added chapter 9 functionality

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['jonathan@cychow.co.uk']
+    POSTS_PER_PAGE = 3
